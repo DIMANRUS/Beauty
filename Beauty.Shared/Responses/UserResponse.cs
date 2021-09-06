f8188a954ddb274f8a1f31678d8305aba6d057b2
DIMANRUS <41239551+DIMANRUS@users.noreply.github.com>
2021-09-06 20:39:41 +0300

    Fix Get Name From JSON

--- Beauty.Shared/Responses/UserResponse.cs
+++ Beauty.Shared/Responses/UserResponse.cs
@@ -1,9 +1,15 @@
-﻿namespace Beauty.Shared.Responses {
+﻿using System.Text.Json.Serialization;
+
+namespace Beauty.Shared.Responses {
     public class UserResponse {
         public string Id { get; set; }
+        [JsonPropertyName("email")]
         public string Email { get; set; }
+        [JsonPropertyName("phoneNumber")]
         public string PhoneNumber { get; set; }   
+        [JsonPropertyName("userName")]
         public string UserName { get; set; }
+        [JsonPropertyName("photo")]
         public byte[] Photo { get; set; }
     }
 }
