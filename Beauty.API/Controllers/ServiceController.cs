50bdc76f5dbcc4678c71d0a07b019d8a2335630e
DIMANRUS <41239551+DIMANRUS@users.noreply.github.com>
2021-10-02 10:24:13 +0300

    Add get Services

---
+++ Beauty.API/Controllers/ServiceController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Beauty.EFDataAccessLibrary.Contexts;
+using Beauty.EFDataAccessLibrary.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace Beauty.API.Controllers
+{
+    [Route("[controller]")]
+    [Authorize]
+    public class ServiceController : Controller
+    {
+        ApplicationDbContext _db;
+        public ServiceController(ApplicationDbContext context)
+        {
+            _db = context;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<ServiceCategory>> GetServicesAndCategories() =>
+            await _db.ServiceCategories.Include(x => x.Services).ToListAsync();
+    }
+}
--- Beauty/ViewModels/BarPagesVM/User/SearchServicePageVM.cs
+++ Beauty/ViewModels/BarPagesVM/User/SearchServicePageVM.cs
@@ -1,11 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
+using Beauty.EFDataAccessLibrary.Models;
 using Beauty.ViewModels.Shared;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using System.Text.Json;
 
 namespace Beauty.ViewModels.BarPagesVM.User
 {
     class SearchServicePageVM : BaseVM
     {
+        public SearchServicePageVM()
+        {
+            PageLoadingCommand = new Command(async () =>
+            {
+                CurrentState = LayoutState.Loading;
+                HttpClient httpClient = new HttpClient();
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await SecureStorage.GetAsync("UserToken"));
+                var json = await httpClient.GetStringAsync("https://api.beauty.dimanrus.ru/Service");
+                ServiceCategories = JsonSerializer.Deserialize<IEnumerable<ServiceCategory>>(json);
+                CurrentState = LayoutState.None;
+            });
+        }
+        public IEnumerable<ServiceCategory> ServiceCategories { get; set; }
+        public Service ServiceSelected { get; set; }
+        public ServiceCategory serviceCategorySelected { get; set; }
+        public ObservableCollection<ServiceWorker> ServicesWorkers { get; private set; }
     }
 }
